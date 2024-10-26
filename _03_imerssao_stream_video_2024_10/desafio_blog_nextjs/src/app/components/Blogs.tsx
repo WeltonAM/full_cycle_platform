94f4f94c76824fa1efc954b664aa74c2bb50e5b9
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-26 11:19:46 -0300

    Carregar mais

--- _03_imerssao_stream_video_2024_10/desafio_blog_nextjs/src/app/components/Blogs.tsx
+++ _03_imerssao_stream_video_2024_10/desafio_blog_nextjs/src/app/components/Blogs.tsx
@@ -3,11 +3,17 @@ import useBlog from "@/data/hooks/useBlog";
 import Blog from "@/data/model/Blog";
 import Link from "next/link";
 import Loading from "./Loading";
+import { useState } from "react";
 
 export default function Blogs() {
     const { blogs } = useBlog();
+    const [visibleCount, setVisibleCount] = useState(5);
 
-    if(!blogs) return <Loading />;
+    if (!blogs) return <Loading />;
+
+    const loadMoreBlogs = () => {
+        setVisibleCount((prevCount) => prevCount + 5); 
+    };
 
     return (
         <div className="flex flex-1 flex-col items-center justify-center py-5 px-36 gap-4">
@@ -21,25 +27,29 @@ export default function Blogs() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="shadow-md rounded-lg p-4">
-                        {blogs.map((blog: Blog) => (
+                    {blogs.slice(0, visibleCount).map((blog: Blog) => (
+                        <tr key={blog.id} className="shadow-md rounded-lg p-4">
                             <Link 
-                                key={blog.id} 
                                 href={`/${blog.id}`} 
-                                className="
-                                    flex items-center gap-4 
-                                    hover:bg-slate-900 hover:text-white
-                                    text-zinc-300
-                                "
+                                className="flex items-center gap-4 hover:bg-slate-900 hover:text-white text-zinc-300"
                             >
                                 <td className="p-4">#{blog.id}</td>
                                 <td className="p-4">{blog.userId}</td>
                                 <td className="p-4">{blog.title}</td>
                             </Link>
-                        ))}
-                    </tr>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
+
+            {visibleCount < blogs.length && (
+                <button
+                    onClick={loadMoreBlogs}
+                    className="mt-4 py-2 px-4 bg-slate-900 text-white rounded-lg"
+                >
+                    Carregar Mais
+                </button>
+            )}
         </div>
     );
 }
