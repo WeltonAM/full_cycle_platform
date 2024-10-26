18620ae62432a522f7782b064bc48b5b22167fc4
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-26 13:49:23 -0300

    Tratativa de erro acho .env não tenha sido configurado

--- _03_imerssao_stream_video_2024_10/desafio_blog_nextjs/src/app/components/Blogs.tsx
+++ _03_imerssao_stream_video_2024_10/desafio_blog_nextjs/src/app/components/Blogs.tsx
@@ -15,41 +15,45 @@ export default function Blogs() {
         setVisibleCount((prevCount) => prevCount + 5); 
     };
 
-    return (
-        <div className="flex flex-1 flex-col items-center justify-center py-5 px-36 gap-4">
-            <h1 className="text-2xl font-bold select-none text-zinc-300">Blogs</h1>
-            <table>
-                <thead className="flex items-center justify-start text-zinc-200">
-                    <tr>
-                        <th className="p-4 font-bold">#</th>
-                        <th className="p-4 font-bold">Autor</th>
-                        <th className="p-4 font-bold">Título</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {blogs.slice(0, visibleCount).map((blog: Blog) => (
-                        <tr key={blog.id} className="shadow-md rounded-lg p-4">
-                            <Link 
-                                href={`/${blog.id}`} 
-                                className="flex items-center gap-4 hover:bg-slate-900 hover:text-white text-zinc-300"
-                            >
-                                <td className="p-4">#{blog.id}</td>
-                                <td className="p-4">{blog.userId}</td>
-                                <td className="p-4">{blog.title}</td>
-                            </Link>
+    if (blogs && blogs.length > 0) {
+        return (
+            <div className="flex flex-1 flex-col items-center justify-center py-5 px-36 gap-4">
+                <h1 className="text-2xl font-bold select-none text-zinc-300">Blogs</h1>
+                <table>
+                    <thead className="flex items-center justify-start text-zinc-200">
+                        <tr>
+                            <th className="p-4 font-bold">#</th>
+                            <th className="p-4 font-bold">Autor</th>
+                            <th className="p-4 font-bold">Título</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {blogs.slice(0, visibleCount).map((blog: Blog) => (
+                            <tr key={blog.id} className="shadow-md rounded-lg p-4">
+                                <Link 
+                                    href={`/${blog.id}`} 
+                                    className="flex items-center gap-4 hover:bg-slate-900 hover:text-white text-zinc-300"
+                                >
+                                    <td className="p-4">#{blog.id}</td>
+                                    <td className="p-4">{blog.userId}</td>
+                                    <td className="p-4">{blog.title}</td>
+                                </Link>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
 
-            {visibleCount < blogs.length && (
-                <button
-                    onClick={loadMoreBlogs}
-                    className="mt-4 py-2 px-4 bg-slate-900 text-white rounded-lg"
-                >
-                    Carregar Mais
-                </button>
-            )}
-        </div>
-    );
+                {visibleCount < blogs.length && (
+                    <button
+                        onClick={loadMoreBlogs}
+                        className="mt-4 py-2 px-4 bg-slate-900 text-white rounded-lg"
+                    >
+                        Carregar Mais
+                    </button>
+                )}
+            </div>
+        );
+    } else {
+        return <div className="pt-36">Configurar o arquivo .env</div>;
+    }
 }
